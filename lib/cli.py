9e930de49f2de66a959fa1c6aed4a466f356ed1b
github username <github email address>
2022-05-17 09:43:28 -0400

    build out cli and coctail class methods

--- lib/cli.py
+++ lib/cli.py
@@ -1,31 +1,61 @@
-import requests
-import json
+from email import message
 from bdb import Breakpoint
 from cocktail import Cocktail
 
 
 class Cli:
     def __init__(self):
-        print("Best of luck!")
-        # get stuff and start process
-
-        response_API = requests.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail')
-        print(response_API.json())
-        cocktails = response_API.json()["drinks"]
-
-        
-        for cocktail in cocktails:
-            Cocktail(cocktail['idDrink'], cocktail['strDrink'])
-
+        Cocktail.get_all_drinks()
         self.start()
 
-        #print(response_API.status_code)
-
     def start(self):
-        print('I am here')
+        print('Welcome to Cocktail Corner - A place to discover delicious new drinks to try!')
+        print(' ')
+        print('Choose a drink from the list below:')
+        print(' ')
 
-        
+        while True:
+            self.print_cocktails()
+            print(' ')
+
+            pick = self.ask_user()
+            if pick == 'exit':
+                break
+
+            drink = Cocktail.all[int(pick) - 1]
+            print(drink.name)
+            print("Yum - great choice!")
+            print(' ')
+            print(f'Mixing your {drink.name} now!')
+            print(' ')
+            if not hasattr(drink, 'instructions'):
+                Cocktail.get_drink_details(Cocktail, drink)
+            # print details
+            print(drink.instructions)
+            print(' ')
+            message = 'Would you like to see another delicious drink? (y/n):   '    
+            pick = input(message)
+            print(' ')
+            if pick == 'n':
+                break
+
+        print(' ')
+        print('Bye!  Drive safely!')
+
+
+    def ask_user(self):  
+            message = "Enter the number of your choice here:   "      
+            pick = input(message)
+            print(' ')
+            while pick != "exit":
+                if int(pick) > 0 and int(pick) <= len(Cocktail.all): 
+                    return pick
+                else:
+                    message = "That was not an allowed choice - Please pick the number of your favorite drink:   "
+                    pick = input(message)
+                    print(' ')
+            return pick
         
     def print_cocktails(self):
             for idx, cocktail in enumerate(Cocktail.all):
-                print(f'{idx}.  {cocktail.name}' )
+                print(f'{idx + 1}.  {cocktail.name}' )
--- lib/models/cocktail.py
+++ lib/models/cocktail.py
@@ -1,11 +1,26 @@
+import requests
+import json
+from bdb import Breakpoint
+
 class Cocktail:
     all = []
-    
+
     def __init__(self, id, name):
         self.id = id
         self.name = name
         Cocktail.all.append(self)
 
+
+    @classmethod
+    def get_all_drinks(cls):
+        response_API = requests.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail')
+        cocktails = response_API.json()["drinks"]
+        for cocktail in cocktails:
+            cls(cocktail['idDrink'], cocktail['strDrink'])
  
+    def get_drink_details(cls, drink):
+        response_API = requests.get(f'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={drink.id}')
+        cocktail = response_API.json()["drinks"][0]
+        drink.instructions = cocktail['strInstructions']
 
     
